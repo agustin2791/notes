9b0d7ab4eddd8ed4bc829a29b1bfd6b06bae1674
Jesus A. Diaz-Barriga <jagustin2791@gmail.com>
2019-02-25 22:18:48 -0800

    sections API

--- settings/start.py
+++ settings/start.py
@@ -12,6 +12,7 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/notesdb'
 
 mongo = PyMongo(app)
 
+# Subject API, gets all subjects and posts new
 class AllSubjects(Resource):
 	def get(self):
 		connect = mongo.db.subject
@@ -24,8 +25,12 @@ class AllSubjects(Resource):
 		connect = mongo.db.subject
 		new_sub = request.json['subject']
 		connect.insert({'subject': new_sub})
-		return jsonify({'success': True})
+		subjects = []
+		for c in connect.find():
+			subjects.append({'id': str(c['_id']), 'subject': c['subject']})
+		return jsonify({'success': True, 'results': subjects})
 
+# Single subject, meant to update and delete subject
 class Subject(Resource):
 	def get(self, subject_id):
 		connect = mongo.db.subject
@@ -35,8 +40,8 @@ class Subject(Resource):
 	def put(self, subject_id):
 		connect = mongo.db.subject
 		edit = request.json['edit']
-		subject = connect.find_one({'_id': ObjectId(subject_id)})
-		connect.update_one(subject, {'$set': {'subject': edit}}, upsert=False)
+		# subject = connect.find_one({'_id': ObjectId(subject_id)})
+		connect.update_one({'_id': ObjectId(subject_id)}, {'$set': {'subject': edit}}, upsert=False)
 		return jsonify({'id': str(subject['_id']), 'subject': subject['subject']})
 
 	def delete(self, subject_id):
@@ -45,6 +50,63 @@ class Subject(Resource):
 		deleted_sub = {'id': str(subject['_id']), 'subject': subject['subject']}
 		connect.delete_one(subject)
 		return jsonify({'deleted': deleted_sub})
+
+# Section API, gets all sections and adds new section
+class AllSections(Resource):
+	def get(self, subject_id):
+		connect = mongo.db.section
+		# sub_sections = connect.find({'subject_id': subject_id})
+		sections = []
+		for s in connect.find({'subject_id': subject_id}):
+			sections.append({
+				'id': str(s['_id']),
+				'section': s['section'],
+				'subject_id': s['subject_id']
+				})
+		return jsonify({'results': sections})
+
+	def post(self, subject_id):
+		connect = mongo.db.section
+		section = request.json['section']
+		connect.insert({'section': section, 'subject_id': subject_id})
+		sections = []
+		for s in connect.find({'subject_id': subject_id}):
+			sections.append({
+				'id': str(s['_id']),
+				'section': s['section'],
+				'subject_id': s['subject_id']
+				})
+		return jsonify({'success': True, 'results': sections})
+
+# Single section, update and delete subject+
+class Section(Resource):
+	def get(self, section_id):
+		connect = mongo.db.section
+		section = connect.find_one({'_id': ObjectId(section_id)})
+		if section:
+			return jsonify({
+				'id': str(section['_id']),
+				'section': section['section'],
+				'subject_id': section['subject_id']
+				})
+		return jsonify({'results': 'No Section Found'})
+
+	def put(self, section_id):
+		connect = mongo.db.section
+		connect.update_one({'_id': ObjectId(section_id)}, {'$set': {'section': request.json['edit']}}, upsert=False)
+		section = connect.find_one({'_id': ObjectId(section_id)})
+		return jsonify({'success': True, 'results': {
+			'id': str(section['_id']),
+			'section': section['section'],
+			'subject_id': section['subject_id']
+			}})
+
+	def delete(self, section_id):
+		connect = mongo.db.section
+		section = connect.find_one({'_id', ObjectId(section_id)})
+		connect.delete_one(section)
+		return jsonify({'result': 'Section Deleted!'})
+
 
 
 # class HelloWorld(Resource):
@@ -62,6 +124,8 @@ class Api(Resource):
 api.add_resource(Api, '/api/<int:num>')
 api.add_resource(AllSubjects, '/api/subjects')
 api.add_resource(Subject, '/api/subjects/<subject_id>')
+api.add_resource(AllSections, '/api/sections/<subject_id>')
+api.add_resource(Section, '/api/section/<section_id>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
