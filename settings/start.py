7caa0a5d8f8d1952def42339cd30a35f50b746e9
Jesus Diaz-Barriga <jagustin2791@gmail.com>
2019-05-02 16:19:52 -0700

    auth start

--- settings/start.py
+++ settings/start.py
@@ -1,28 +1,49 @@
 from flask import Flask, jsonify, request, url_for, session, redirect
 from flask_restful import Resource, Api
-from flask_pymongo import PyMongo
-# import bcrypt
+from flask_mongoengine import MongoEngine
+# from flask_pymongo import PyMongo, MongoClient
+from flask_user import login_required, UserManager, UserMixin
+import bcrypt
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 api = Api(app)
 
-# app.config['SCRETE_KEY'] = 'thisissecret'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/notesdb'
+# app.config['SCRETE_KEY'] = 'Iy4qLzM1WeHzhJVTbKF0jplDgQS3p8Jl'
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/notesdb'
+db = MongoEngine(app)
 
-mongo = PyMongo(app)
+class ConfigClass(object):
+	SECRET_KEY = 'Iy4qLzM1WeHzhJVTbKF0jplDgQS3p8Jl'
+	MONGODB_SETTINGS = {
+		'db': 'notesdb',
+		'host': 'mongodb://localhost:27017/notesdb'
+	}
+	USER_APP_NAME = 'Notes App'
+	USER_ENABLE_EMAIL = False
+	USER_ENABLE_USERNAME = True
+	USER_REQUIRE_RETYPE_PASSWORD = False
+
+class User(db.Document, UserMixin):
+	meta = {'collection': 'admin'}
+	active = db.BooleanField(default=True)
+	email = db.StringField(max_length=50)
+	password = db.StringField()
+
+app.config.from_object(__name__+'.ConfigClass')
+user_manager = UserManager(app, db, User)
 
 # Subject API, gets all subjects and posts new
 class AllSubjects(Resource):
 	def get(self):
-		connect = mongo.db.subject
+		connect = db.db.subject
 		subjects = []
 		for c in connect.find():
 			subjects.append({'id': str(c['_id']), 'subject': c['subject']})
 		return jsonify({'results': subjects})
 
 	def post(self):
-		connect = mongo.db.subject
+		connect = db.db.subject
 		new_sub = request.json['subject']
 		connect.insert({'subject': new_sub})
 		subjects = []
@@ -33,19 +54,19 @@ class AllSubjects(Resource):
 # Single subject, meant to update and delete subject
 class Subject(Resource):
 	def get(self, subject_id):
-		connect = mongo.db.subject
+		connect = db.db.subject
 		subject = connect.find_one({'_id': ObjectId(subject_id)})
-		return jsonify({'results': {'id': str(subject['_id']), 'subejct': subject['subject']}})
+		return jsonify({'results': {'id': str(subject['_id']), 'subeject': subject['subject']}})
 
 	def put(self, subject_id):
-		connect = mongo.db.subject
+		connect = db.db.subject
 		edit = request.json['edit']
-		# subject = connect.find_one({'_id': ObjectId(subject_id)})
+		subject = connect.find_one({'_id': ObjectId(subject_id)})
 		connect.update_one({'_id': ObjectId(subject_id)}, {'$set': {'subject': edit}}, upsert=False)
 		return jsonify({'id': str(subject['_id']), 'subject': subject['subject']})
 
 	def delete(self, subject_id):
-		connect = mongo.db.subject
+		connect = db.db.subject
 		subject = connect.find_one({'_id': ObjectId(subject_id)})
 		deleted_sub = {'id': str(subject['_id']), 'subject': subject['subject']}
 		connect.delete_one(subject)
@@ -54,7 +75,7 @@ class Subject(Resource):
 # Section API, gets all sections and adds new section
 class AllSections(Resource):
 	def get(self, subject_id):
-		connect = mongo.db.section
+		connect = db.db.section
 		# sub_sections = connect.find({'subject_id': subject_id})
 		sections = []
 		for s in connect.find({'subject_id': subject_id}):
@@ -66,7 +87,7 @@ class AllSections(Resource):
 		return jsonify({'results': sections})
 
 	def post(self, subject_id):
-		connect = mongo.db.section
+		connect = db.db.section
 		section = request.json['section']
 		connect.insert({'section': section, 'subject_id': subject_id})
 		sections = []
@@ -81,7 +102,7 @@ class AllSections(Resource):
 # Single section, update and delete subject+
 class Section(Resource):
 	def get(self, section_id):
-		connect = mongo.db.section
+		connect = db.db.section
 		section = connect.find_one({'_id': ObjectId(section_id)})
 		if section:
 			return jsonify({
@@ -92,7 +113,7 @@ class Section(Resource):
 		return jsonify({'results': 'No Section Found'})
 
 	def put(self, section_id):
-		connect = mongo.db.section
+		connect = db.db.section
 		connect.update_one({'_id': ObjectId(section_id)}, {'$set': {'section': request.json['edit']}}, upsert=False)
 		section = connect.find_one({'_id': ObjectId(section_id)})
 		return jsonify({'success': True, 'results': {
@@ -102,7 +123,7 @@ class Section(Resource):
 			}})
 
 	def delete(self, section_id):
-		connect = mongo.db.section
+		connect = db.db.section
 		section = connect.find_one({'_id', ObjectId(section_id)})
 		connect.delete_one(section)
 		return jsonify({'result': 'Section Deleted!'})
